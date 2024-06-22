35b354ee607626ebefb551559b4cbb0bd426d942
Star <upc.chihanyang@gmail.com>
2024-06-22 22:54:27 +0800

    Add files via upload

---
+++ ABD-main/code/dataloaders/dataset.py
@@ -0,0 +1,249 @@
+import os
+# import cv2
+import torch
+import random
+import numpy as np
+from glob import glob
+from torch.utils.data import Dataset
+import h5py
+from scipy.ndimage.interpolation import zoom
+from torchvision import transforms
+import itertools
+from scipy import ndimage
+from torch.utils.data.sampler import Sampler
+import matplotlib.pyplot as plt
+from PIL import Image
+from PIL import ImageFilter
+
+class BaseDataSets(Dataset):
+    def __init__(
+            self,
+            base_dir=None,
+            split="train",
+            num=None,
+            transform=None,
+            ops_weak=None,
+            ops_strong=None,
+    ):
+        self._base_dir = base_dir
+        self.sample_list = []
+        self.split = split
+        self.transform = transform
+        self.ops_weak = ops_weak
+        self.ops_strong = ops_strong
+
+        assert bool(ops_weak) == bool(
+            ops_strong
+        ), "For using CTAugment learned policies, provide both weak and strong batch augmentation policy"
+
+        if self.split == "train":
+            with open(self._base_dir + "/train_slices.list", "r") as f1:
+                self.sample_list = f1.readlines()
+            self.sample_list = [item.replace("\n", "") for item in self.sample_list]
+            print("Train total {} samples".format(len(self.sample_list)))
+
+        elif self.split == "val":
+            with open(self._base_dir + "/val.list", "r") as f:
+                self.sample_list = f.readlines()
+            self.sample_list = [item.replace("\n", "") for item in self.sample_list]
+            print("Val total {} samples".format(len(self.sample_list)))
+        if num is not None and self.split == "train":
+            self.sample_list = self.sample_list[:num]
+        
+
+    def __len__(self):
+        return len(self.sample_list)
+
+    def __getitem__(self, idx):
+        case = self.sample_list[idx]
+        if self.split == "train":
+            h5f = h5py.File(self._base_dir + "/data/slices/{}.h5".format(case), "r")
+        else:
+            h5f = h5py.File(self._base_dir + "/data/{}.h5".format(case), "r")
+        image = h5f["image"][:]
+        label = h5f["label"][:]
+        sample = {"image": image, "label": label}
+        if self.split == "train":
+            if None not in (self.ops_weak, self.ops_strong):
+                sample = self.transform(sample, self.ops_weak, self.ops_strong)
+            else:
+                sample = self.transform(sample)
+        sample["idx"] = idx
+        return sample
+
+def random_rot_flip(image, label=None):
+    k = np.random.randint(0, 4)  
+    image = np.rot90(image, k)  
+    axis = np.random.randint(0, 2)  
+    image = np.flip(image, axis=axis).copy()  
+    if label is not None:  
+        label = np.rot90(label, k)  
+        label = np.flip(label, axis=axis).copy()  
+        return image, label
+    else:
+        return image
+
+def random_rotate(image, label):
+    angle = np.random.randint(-20, 20)  
+    image = ndimage.rotate(image, angle, order=0, reshape=False)  
+    label = ndimage.rotate(label, angle, order=0, reshape=False)  
+    return image, label
+
+def blur(image,p=0.5):
+    if random.random()<p:
+        max = np.max(image)
+        min = np.min(image)
+        sigma = np.random.uniform(0.1,2.0)
+        image = Image.fromarray(((image-min)/(max-min)*255).astype('uint8'))
+        image = np.array(image.filter(ImageFilter.GaussianBlur(radius=sigma)))
+        image = min + image*(max-min)/255
+    return image
+def color_jitter(image):
+    if not torch.is_tensor(image):
+        np_to_tensor = transforms.ToTensor()
+        image = np_to_tensor(image)
+    # s is the strength of color distortion.
+    s = 1.0
+    jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+    return jitter(image)
+
+def cutout_gray(img, mask, p=0.5, size_min=0.02, size_max=0.4, ratio_1=0.3, ratio_2=1/0.3, value_min=0, value_max=1, pixel_level=True):
+    if random.random() < p:
+        img = np.array(img)
+        mask = np.array(mask)
+
+        img_h, img_w = img.shape
+
+        while True:
+            size = np.random.uniform(size_min, size_max) * img_h * img_w
+            ratio = np.random.uniform(ratio_1, ratio_2)
+            erase_w = int(np.sqrt(size / ratio))
+            erase_h = int(np.sqrt(size * ratio))
+            x = np.random.randint(0, img_w)
+            y = np.random.randint(0, img_h)
+
+            if x + erase_w <= img_w and y + erase_h <= img_h:
+                break
+
+        if pixel_level:
+            value = np.random.randint(value_min, value_max + 1, (erase_h, erase_w))
+        else:
+            value = np.random.randint(value_min, value_max + 1)
+
+        img[y:y + erase_h, x:x + erase_w] = value
+        mask[y:y + erase_h, x:x + erase_w] = 0
+
+    return img, mask
+
+class RandomGenerator(object):
+    def __init__(self, output_size):
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label = sample["image"], sample["label"]
+        # ind = random.randrange(0, img.shape[0])
+        # image = img[ind, ...]
+        # label = lab[ind, ...]
+        if random.random() > 0.5:  
+            image, label = random_rot_flip(image, label)
+        elif random.random() > 0.5:
+            image, label = random_rotate(image, label)
+        x, y = image.shape
+        image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y),order=0)  
+        label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
+        image = torch.from_numpy(image.astype(np.float32)).unsqueeze(0)  
+        label = torch.from_numpy(label.astype(np.uint8))
+        sample = {"image": image, "label": label}
+        return sample
+
+
+class WeakStrongAugment(object):
+    """returns weakly and strongly augmented images
+    Args:
+        object (tuple): output size of network
+    """
+    def __init__(self, output_size):
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label = sample["image"], sample["label"]
+        if random.random() > 0.5:  
+            image, label = random_rot_flip(image, label)
+        elif random.random() > 0.5:
+            image, label = random_rotate(image, label)
+        # weak augmentation is rotation / flip
+        x, y = image.shape
+        image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y),order=0)  
+        label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
+
+        # strong augmentation is color jitter
+        image_strong, label_strong = cutout_gray(image,label,p=0.5)
+        image_strong = color_jitter(image_strong).type("torch.FloatTensor")
+        # image_strong = blur(image, p=0.5)
+        image = torch.from_numpy(image.astype(np.float32)).unsqueeze(0)
+        # image_strong = torch.from_numpy(image_strong.astype(np.float32)).unsqueeze(0)
+
+        label = torch.from_numpy(label.astype(np.uint8))
+        label_strong = torch.from_numpy(label_strong.astype(np.uint8))
+        sample = {
+            "image": image,
+            "image_strong": image_strong,
+            "label": label,
+            "label_strong": label_strong
+        }
+        return sample
+
+    def resize(self, image):
+        x, y = image.shape
+        return zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=0)
+
+
+class TwoStreamBatchSampler(Sampler):
+    """Iterate two sets of indices
+
+    An 'epoch' is one iteration through the primary indices.
+    During the epoch, the secondary indices are iterated through
+    as many times as needed.
+    """
+
+    def __init__(self, primary_indices, secondary_indices, batch_size, secondary_batch_size):
+        self.primary_indices = primary_indices
+        self.secondary_indices = secondary_indices
+        self.secondary_batch_size = secondary_batch_size
+        self.primary_batch_size = batch_size - secondary_batch_size
+
+        assert len(self.primary_indices) >= self.primary_batch_size > 0
+        assert len(self.secondary_indices) >= self.secondary_batch_size > 0
+
+    def __iter__(self):
+        primary_iter = iterate_once(self.primary_indices)
+        secondary_iter = iterate_eternally(self.secondary_indices)
+        return (
+            primary_batch + secondary_batch
+            for (primary_batch, secondary_batch) in zip(
+            grouper(primary_iter, self.primary_batch_size),
+            grouper(secondary_iter, self.secondary_batch_size),
+        )
+        )
+
+    def __len__(self):
+        return len(self.primary_indices) // self.primary_batch_size
+
+
+def iterate_once(iterable):
+    return np.random.permutation(iterable)
+
+
+def iterate_eternally(indices):
+    def infinite_shuffles():
+        while True:
+            yield np.random.permutation(indices)
+
+    return itertools.chain.from_iterable(infinite_shuffles())
+
+
+def grouper(iterable, n):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3) --> ABC DEF"
+    args = [iter(iterable)] * n
+    return zip(*args)
